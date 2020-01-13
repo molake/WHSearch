55bb3ad17113bb7e4bdba9ba748874c75c7e2560
Hao <hao.d.wu@accenture.com>
2020-01-13 11:40:19 +0800

    update

--- Example/Pods/Target Support Files/Pods-WHSearch_Example/Pods-WHSearch_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-WHSearch_Example/Pods-WHSearch_Example-frameworks.sh
@@ -161,10 +161,14 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/PanModal/PanModal.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/WHSearch/WHSearch.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/WHUI/WHUI.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/PanModal/PanModal.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/WHSearch/WHSearch.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/WHUI/WHUI.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
