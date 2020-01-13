036a3a6a9ab67470d1cb398f95b487b020184c62
Hao <hao.d.wu@accenture.com>
2020-01-13 11:40:28 +0800

    update

---
+++ Example/Pods/WHUI/WHUI/Classes/WHButton.swift
@@ -0,0 +1,29 @@
+//
+//  WHButton.swift
+//  Test
+//
+//  Created by wu, hao on 2020/1/10.
+//  Copyright © 2020 wuhao. All rights reserved.
+//
+
+import UIKit
+
+open class WHButton: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
+
+public class WHView: UIView {
+    
+}
+
+public class WHLabel: UILabel {
+    
+}
